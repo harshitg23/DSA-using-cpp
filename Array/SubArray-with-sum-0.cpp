fde06b13e1078ff4e2063d3dec09c0e2927456df
HARSHIT GOYAL <69359941+harshitg23@users.noreply.github.com>
2021-07-13 14:05:27 +0530

    Add files via upload

---
+++ Array/SubArray-with-sum-0.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        unordered_set<int> st;
+        int sum = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] == 0)
+                return true;
+            sum += arr[i];
+
+            if(sum == 0 || st.find(sum) != st.end())
+                return true;
+            st.insert(sum);
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}  // } Driver Code Ends
