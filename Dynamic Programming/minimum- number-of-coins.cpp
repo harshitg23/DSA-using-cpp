8f9232b8cecda433f479e6f65d8b483c9282cd33
HARSHIT GOYAL <69359941+harshitg23@users.noreply.github.com>
2021-07-21 12:18:03 +0530

    Add files via upload

---
+++ Dynamic Programming/minimum- number-of-coins.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minCoins(int coins[], int n, int amount)
+{
+    int dp[n+1][amount+1];
+
+    for(int i=0; i<=n; i++)
+    {
+        for(int j=0; j<=amount; j++)
+        {
+            if(i==0)
+                dp[i][j] = 1e5;
+            else if(j==0)
+                dp[i][j] = 0;
+            else if(coins[i-1] > j)
+                dp[i][j] = dp[i-1][j];
+            else
+                dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+        }
+    }
+    return dp[n][amount]>1e4 ? -1 : dp[n][amount];
+}
+int main()
+{
+    int sum,n;
+    cin>>sum>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    int ans = minCoins(arr,n,sum);
+    cout<<ans;
+    return 0;
+}
