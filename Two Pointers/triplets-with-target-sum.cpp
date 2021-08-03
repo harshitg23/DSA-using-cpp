f9e9c0017e6a1c66d1b9076a349627fba40e456c
HARSHIT GOYAL <69359941+harshitg23@users.noreply.github.com>
2021-08-03 15:46:11 +0530

    Add files via upload

---
+++ Two Pointers/triplets-with-target-sum.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums, int target)
+{
+    int n =nums.size();
+    vector<vector<int>> ans;
+    set<vector<int>> st;
+    sort(nums.begin(), nums.end());
+    for(int i=0; i<n-2; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        int left = i+1, right = n-1;
+        while(left<right)
+        {
+            int current = nums[i] + nums[left] + nums[right];
+            if(current == target)
+            {
+                vector<int> v;
+                v.push_back(nums[i]);
+                v.push_back(nums[left]);
+                v.push_back(nums[right]);
+                st.insert(v);
+                left++;
+                right--;
+            }
+            else if(current < target)
+                left++;
+            else
+                right--;
+        }
+    }
+
+    for(auto x : st)
+        ans.push_back(x);
+    return ans;
+}
+
+int main()
+{
+    int n, target;
+    cin>>n>>target;
+    vector<int> nums(n);
+    for(int i=0; i<n; i++)
+        cin>>nums[i];
+    vector<vector<int>> v = threeSum(nums, target);
+    for(int i=0; i<v.size(); i++)
+    {
+        cout<<"["<<v[i][0]<<", "<<v[i][1]<<", "<<v[i][2]<<"]"<<endl;
+    }
+    return 0;
+}
